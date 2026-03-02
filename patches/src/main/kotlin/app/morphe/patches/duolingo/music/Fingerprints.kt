0cbcd836c27a483119730554d27312cf9efacea8
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-03-02 08:32:02 -0800

    fix(Duolingo): Remove `Unlock licensed songs` patch

--- patches/src/main/kotlin/app/morphe/patches/duolingo/music/Fingerprints.kt
+++
@@ -1,8 +0,0 @@
-package app.morphe.patches.duolingo.music
-
-import app.morphe.patcher.Fingerprint
-
-// Matches toString()
-object MusicCourseFingerprint : Fingerprint (
-    strings = listOf("Music(courseSummary=", ", activePathSectionId=")
-)
