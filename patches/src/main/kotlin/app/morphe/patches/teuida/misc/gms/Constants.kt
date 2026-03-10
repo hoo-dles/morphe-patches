efba5b276d0713e3efc108f706d1f46cd9d67b5e
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-03-10 10:28:27 -0700

    fix(Teuida): Update patches to latest (v1.21.16)

--- patches/src/main/kotlin/app/morphe/patches/teuida/misc/gms/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/teuida/misc/gms/Constants.kt
@@ -3,4 +3,5 @@ package app.morphe.patches.teuida.misc.gms
 internal object Constants {
     const val TEUIDA_PACKAGE_NAME = "net.teuida.teuida"
     const val MORPHE_TEUIDA_PACKAGE_NAME = "app.morphe.teuida"
+    const val MAIN_ACTIVITY_NAME = "/EntryActivity;"
 }
