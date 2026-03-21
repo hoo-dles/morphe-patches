cad792ecfcc17c297a3b02b4637c139585af5356
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-03-21 09:25:45 -0700

    fix(Daily Pocket): Update MicroG integration in conjunction with MicroG RE fixes

--- patches/src/main/kotlin/app/morphe/patches/dailypocket/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/dailypocket/misc/gms/GmsCoreSupportPatch.kt
@@ -1,33 +1,11 @@
 package app.morphe.patches.dailypocket.misc.gms
 
-import app.morphe.patches.dailypocket.misc.gms.Constants.DAILYPAY_PACKAGE_NAME
-import app.morphe.patches.dailypocket.misc.gms.Constants.MORPHE_DAILYPAY_PACKAGE_NAME
-import app.morphe.patches.shared.misc.extension.activityOnCreateExtensionHook
-import app.morphe.patches.shared.misc.extension.sharedExtensionPatch
 import app.morphe.patches.shared.misc.gms.gmsCoreSupportPatch
-import app.morphe.patches.shared.misc.gms.gmsCoreSupportResourcePatch
-import app.morphe.util.indexOfFirstInstruction
-import com.android.tools.smali.dexlib2.Opcode
 
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
-    fromPackageName = DAILYPAY_PACKAGE_NAME,
-    toPackageName = MORPHE_DAILYPAY_PACKAGE_NAME,
-    mainActivityOnCreateFingerprint = MainActivityOnCreateFingerprint,
-    insertIndexResolver = { method -> method.indexOfFirstInstruction(Opcode.INVOKE_SUPER) + 1},
-    extensionPatch = sharedExtensionPatch(
-        activityOnCreateExtensionHook(
-            "/MainActivity;",
-        )
-    ),
-    gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
+    mainActivityName = "/MainActivity;",
+    spoofedPackageSignature = "838ec2c270f8ca934e03b4894f4c15b6a4a4f3fd",
 ) {
-    compatibleWith(DAILYPAY_PACKAGE_NAME)
+    compatibleWith("kr.co.yjteam.dailypay")
 }
-
-private fun gmsCoreSupportResourcePatch() =
-    gmsCoreSupportResourcePatch(
-        fromPackageName = DAILYPAY_PACKAGE_NAME,
-        toPackageName = MORPHE_DAILYPAY_PACKAGE_NAME,
-        spoofedPackageSignature = "838ec2c270f8ca934e03b4894f4c15b6a4a4f3fd",
-)
