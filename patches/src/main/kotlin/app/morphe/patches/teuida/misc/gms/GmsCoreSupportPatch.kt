7304719268e1594a2c46f0a20a398623656505cf
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-03-21 09:26:55 -0700

    fix(Teuida): Update MicroG integration in conjunction with MicroG RE fixes

--- patches/src/main/kotlin/app/morphe/patches/teuida/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/teuida/misc/gms/GmsCoreSupportPatch.kt
@@ -1,30 +1,11 @@
 package app.morphe.patches.teuida.misc.gms
 
-import app.morphe.patches.shared.misc.extension.activityOnCreateExtensionHook
-import app.morphe.patches.shared.misc.extension.sharedExtensionPatch
-import app.morphe.patches.shared.misc.gms.getMainOnCreateFingerprint
 import app.morphe.patches.shared.misc.gms.gmsCoreSupportPatch
-import app.morphe.patches.shared.misc.gms.gmsCoreSupportResourcePatch
-import app.morphe.patches.teuida.misc.gms.Constants.MAIN_ACTIVITY_NAME
-import app.morphe.patches.teuida.misc.gms.Constants.MORPHE_TEUIDA_PACKAGE_NAME
-import app.morphe.patches.teuida.misc.gms.Constants.TEUIDA_PACKAGE_NAME
 
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
-    fromPackageName = TEUIDA_PACKAGE_NAME,
-    toPackageName = MORPHE_TEUIDA_PACKAGE_NAME,
-    mainActivityOnCreateFingerprint = getMainOnCreateFingerprint(MAIN_ACTIVITY_NAME),
-    gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
-    extensionPatch = sharedExtensionPatch(
-        activityOnCreateExtensionHook(MAIN_ACTIVITY_NAME)
-    )
+    mainActivityName = "/EntryActivity;",
+    spoofedPackageSignature = "67bd96b0fff3989af9ed068ec9bd9bbe7583b03b",
 ) {
-    compatibleWith(TEUIDA_PACKAGE_NAME("1.21.16"))
+    compatibleWith("net.teuida.teuida")
 }
-
-private fun gmsCoreSupportResourcePatch() =
-    gmsCoreSupportResourcePatch(
-        fromPackageName = TEUIDA_PACKAGE_NAME,
-        toPackageName = MORPHE_TEUIDA_PACKAGE_NAME,
-        spoofedPackageSignature = "67bd96b0fff3989af9ed068ec9bd9bbe7583b03b",
-)
