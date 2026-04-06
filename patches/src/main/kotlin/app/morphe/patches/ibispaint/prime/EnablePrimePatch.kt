4136c88eff5e96c5ce522cd201595b1f8b6d26fa
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-04-06 12:52:51 -0700

    feat(IbisPaint X): Update patch to support 14.0.0

--- patches/src/main/kotlin/app/morphe/patches/ibispaint/prime/EnablePrimePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/ibispaint/prime/EnablePrimePatch.kt
@@ -14,7 +14,7 @@ val enablePrimePatch = rawResourcePatch(
         name = "IbisPaint X",
         packageName = "jp.ne.ibis.ibispaintx.app",
         appIconColor = 0x0B649B,
-        targets = listOf(AppTarget("13.1.19"))
+        targets = listOf(AppTarget("14.0.0"))
     ))
 
     dependsOn(
@@ -22,10 +22,10 @@ val enablePrimePatch = rawResourcePatch(
             val libPath = "lib/arm64-v8a/libibispaint.so"
             
             // disable anti-tamper
-            "88 9F 00 D0 08 89 45 F9 08 FD DF 88 1F 05 00 71" asPatternTo "E0 03 1F 2A C0 03 5F D6 08 FD DF 88 1F 05 00 71" inFile libPath
+            "28 AE 00 90 08 75 47 F9 08 FD DF 88 1F 05 00 71" asPatternTo "E0 03 1F 2A C0 03 5F D6 08 FD DF 88 1F 05 00 71" inFile libPath
 
             // enable prime membership
-            "48 69 41 94 00 00 62 9E 00 21 60 1E E0 B7 9F 1A 02 00 00 14 E0 03 1F 2A" asPatternTo "48 69 41 94 00 00 62 9E 00 21 60 1E E0 B7 9F 1A 1F 20 03 D5 20 00 80 52" inFile libPath
+            "60 4D 46 94 00 00 62 9E 00 21 60 1E E0 B7 9F 1A 02 00 00 14 E0 03 1F 2A" asPatternTo "60 4D 46 94 00 00 62 9E 00 21 60 1E E0 B7 9F 1A 1F 20 03 D5 20 00 80 52" inFile libPath
         })
     )
 }
