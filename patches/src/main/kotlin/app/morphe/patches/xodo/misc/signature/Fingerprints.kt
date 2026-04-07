cf41fee2b4da2199537e7b26def78525e07c1ef5
hoodles <207470673+hoo-dles@users.noreply.github.com>
2026-04-07 12:03:51 -0700

    feat(Xodo): Add `Enable Pro` patch

---
+++ patches/src/main/kotlin/app/morphe/patches/xodo/misc/signature/Fingerprints.kt
@@ -0,0 +1,7 @@
+package app.morphe.patches.xodo.misc.signature
+
+import app.morphe.patcher.Fingerprint
+
+object SignatureCheckFingerprint : Fingerprint(
+    strings = listOf("completeReaderMainActivity", "completeReaderMainActivity.application")
+)
