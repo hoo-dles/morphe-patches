61bd8dc371ae1c802b4e2a115e1b66abed5823ea
hoo-dles <207470673+hoo-dles@users.noreply.github.com>
2026-09-20 22:40:28 -0700

    feat(Bend): Update support for `7.8.0`

--- patches/src/main/kotlin/hoodles/morphe/patches/bend/premium/EnablePremiumPatch.kt
+++ patches/src/main/kotlin/hoodles/morphe/patches/bend/premium/EnablePremiumPatch.kt
@@ -8,7 +8,7 @@ package hoodles.morphe.patches.bend.premium
 import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 import app.morphe.patcher.patch.bytecodePatch
-import hoodles.morphe.patches.bend.shared.pairip.stripPairipPatch
+import hoodles.morphe.patches.bend.shared.stripPairipPatch
 import hoodles.morphe.patches.shared.revenuecat.getAddEntitlementPatch
 import hoodles.morphe.util.requireArm64
 
@@ -21,7 +21,7 @@ val enablePremiumPatch = bytecodePatch(
         name = "Bend",
         packageName = "com.bowerydigital.bend",
         appIconColor = 0x1674a8,
-        targets = listOf(AppTarget("7.6.6"))
+        targets = listOf(AppTarget("7.8.0"))
     ))
 
     availability(requireArm64())
